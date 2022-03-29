cceeddce78bf31bedd4a4e4a0475dd7cce59733c
Ali Hassaine <ali.hs@outlook.fr>
2022-03-29 22:05:43 +0000

    Add title field to Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_193944) do
+ActiveRecord::Schema.define(version: 2022_03_29_220419) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
