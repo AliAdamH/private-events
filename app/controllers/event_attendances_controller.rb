e9b176a6dad14157b853b56e731f2d80bd511cd7
Ali Hassaine <ali.hs@outlook.fr>
2022-03-30 23:34:17 +0000

    Add event attendance controller

---
+++ app/controllers/event_attendances_controller.rb
@@ -0,0 +1,17 @@
+class EventAttendancesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @event_attendance = current_user.attended_events.build
+  end
+
+  def create
+    @event_attendance = current_user.attended_events.build(event_attendance_params)
+  end
+
+  private
+  
+  def event_attendance_params
+    params.require(:event_attendance).permit(:attended_event_id)
+  end
+end
