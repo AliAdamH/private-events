fb38c1058a80311561a1ac98e6f81d8e1150d76c
Ali Hassaine <ali.hs@outlook.fr>
2022-03-30 11:50:36 +0000

    Whitelist :description param

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:date, :location, :title)
+      params.require(:event).permit(:date, :location, :title, :description)
     end
 end
