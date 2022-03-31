ca8c3bc7de4e2afd3ef632ca6f4fac6359b961d3
Ali Hassaine <ali.hs@outlook.fr>
2022-03-31 11:08:30 +0000

    Fix event attendance method call

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -2,15 +2,15 @@ class EventAttendancesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @event_attendance = current_user.attended_events.build
+    @event_attendance = current_user.event_attendances.build
   end
 
   def create
-    @event_attendance = current_user.attended_events.build(event_attendance_params)
+    @event_attendance = current_user.event_attendances.build(event_attendance_params)
   end
 
   private
-  
+
   def event_attendance_params
     params.require(:event_attendance).permit(:attended_event_id)
   end
