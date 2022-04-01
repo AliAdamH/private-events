f579b695dc2f57a3382033da93ceebe53a8704bd
Ali Hassaine <ali.hs@outlook.fr>
2022-04-01 13:15:22 +0000

    Update user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:attended_events).find(current_user.id)
     @user_events = @user.created_events
   end
-
 end
