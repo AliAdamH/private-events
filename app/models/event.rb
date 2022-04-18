c650bcc119110fbf5045618bf11ae166cdf28a2a
Ali Hassaine <ali.hs@outlook.fr>
2022-04-18 14:51:30 +0000

    Remove useless source option

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
-  has_many :attendees, through: :event_attendances, source: :attendee
+  has_many :attendees, through: :event_attendances
 
 
   scope :past, -> { where('date < ?', Date.today) }
